98020461d0388f24a81d5b771cd8584e71141f2b
anjani1403 <144512903+anjani1403@users.noreply.github.com>
2025-10-14 08:04:57 +0000

    Iterations and Array Methods in JS

---
+++ 05_iterations/three.js
@@ -0,0 +1,41 @@
+// for of loop / enhanced for loop
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "Hello World!"
+// for (const greet of greetings) {
+//     if(greet == " ") continue;
+//     console.log(`Each char is ${greet}`);
+// }
+
+//Maps
+
+// const map = new Map()
+
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('UK', "United Kingdom")
+// map.set('IN', "India")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+const myObject = {
+    'game1' : 'NFS',
+    'game2' : 'Spiderman'
+}
+
+for (const [key, value] of myObject) {  //this loop is not working for objects
+    console.log(key, ':-', value);
+}
